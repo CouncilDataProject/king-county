db83d2d39a274501f5e36022175330fb8afc3859
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-01-11 13:22:59 -0800

    Update dependabot and cdp-scrapers

--- python/setup.py
+++ python/setup.py
@@ -18,7 +18,7 @@ dev_requirements = [
 
 requirements = [
     "cdp-backend[pipeline]==3.0.2",
-    "cdp-scrapers[king_county]>=0.3.2",
+    "cdp-scrapers[king_county]>=0.3.4",
 ]
 
 extra_requirements = {
